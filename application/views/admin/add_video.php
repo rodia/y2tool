2a780acaa57804f1710ff410506a855502b82d2b
César Israel Jaldin Peña <rodia.pierdra@gmail.com>
2013-07-11 19:30:23 -0400

    Update add playlist by multiple videos

--- application/views/admin/add_video.php
+++ application/views/admin/add_video.php
@@ -1,8 +1,36 @@
 <?php
+/**
+ * @version 1.1
+ *
+ */
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 ?>
+<script type="text/javascript">
+	$(function() {
 
+		$('.addInput').live('click', function() {
+			var select_action = $(this).attr("rel");
+
+			var inputsDiv = $('#' + select_action);
+			var i = $('#' + select_action + ' p').size() + 1;
+			var name_field = select_action.replace("_inputs", "");
+			$('<p><label for="video_id"><span>Video ID ' + i + ' * </span><input type="text" class="inp-form" size="50" name="' + name_field + '_ids[]" id="video_ids_' + i + '" value="" placeholder="Enter youtube video id" /></label> <span><a href="#" class="remInput" rel="' + select_action + '" style="color:#0093F0">Remove</a></span></p>').appendTo(inputsDiv);
+			$(inputsDiv).attr("title", i);
+			return false;
+		});
+
+		$('.remInput').live('click', function() {
+			var select_action = $(this).attr("rel");
+			var i = $('#' + select_action + ' p').size() + 1;
+			if( i > 2 ) {
+				$(this).parents('p').remove();
+				i--;
+			}
+			return false;
+		});
+	});
+</script>
 <center>
 	<?php echo form_open("video/add_video_playlist/{$user_id}/{$videoFeedID}", array("method" => "post")); ?>
 	<!--video/addvideo-->
@@ -18,18 +46,21 @@ if (!defined('BASEPATH'))
                     <h2>Youtube video id:</h2>
                 </td>
                 <td>
-					<?php echo form_input(array("name" => "video_id", "placeholder" => "Enter youtube video id", "class" => "inp-form", "size" => 60)); ?>
-                    <span><?php echo form_error('video_id'); ?></span><br/>
-                    <span class="url-demo">e.g. http://www.youtube.com/watch?v=</span><b><i>HcTrHo4dk4Q</i></b>
-
-					<?php echo form_hidden("type", "youtube"); ?>
-					<?php echo form_hidden("videoFeedID", $videoFeedID); ?>
-					<?php echo form_hidden("user_id", $user_id); ?>
-					<?php echo form_hidden("channel", $channel); ?>
+					<div id="like_inputs" title="1">
+						<p><?php echo form_input(array("name" => "video_id", "placeholder" => "Enter youtube video id", "class" => "inp-form", "size" => 60)); ?>
+						<span><?php echo form_error('video_id'); ?></span><br/>
+						<span class="url-demo">e.g. http://www.youtube.com/watch?v=</span><b><i>HcTrHo4dk4Q</i></b>
+						</p>
+					</div>
+					<h2><a href="#" class="addInput" rel="like_inputs" style="color:#0093F0">Add another input box</a></h2>
                 </td>
             </tr>
             <tr>
                 <td colspan="2" align="center">
+					<?php echo form_hidden("type", "youtube"); ?>
+					<?php echo form_hidden("videoFeedID", $videoFeedID); ?>
+					<?php echo form_hidden("user_id", $user_id); ?>
+					<?php echo form_hidden("channel", $channel); ?>
 					<?php echo form_submit(array("name" => "submit", "id" => "button", "class" => "form-submit"), "Add video"); ?>
                 </td>
             </tr>
