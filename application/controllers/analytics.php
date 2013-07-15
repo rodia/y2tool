9d462198f12cb41d07f1c8583ee417c23a508ba2
luis <luis@sozo>
2013-07-15 17:03:58 -0400

    analytics

--- application/controllers/analytics.php
+++ application/controllers/analytics.php
@@ -14,9 +14,15 @@ class Analytics extends CI_Controller {
 	 *
 	 * By default, video dashboard is showed to logged in admin
 	 */
-    function index() {
+    function index($name = "all", $youtube = "all", $country = "all", $category = "all", $sex = "all") {
     	$this->load->library('pagination');
     	//$this->pagination->initialize($opcions);
+    	$page["users"] = $this->user_model->get_all_users($start, $search_name, $search_youtube, $search_country, $search_category, $search_sex);
+    	
+    	$temp_users = explode(",", $this->input->cookie("hold-users"));
+    	$this->video_model->get_temp_users_id($page["hold_users"], $page["pair_user_login"], $temp_users);
+    	 
+    	
     	$page['pagination'] = $this->pagination->create_links();
 		$page['page_name'] = 'list-channels-analytics';
         $page['title'] = "Select Channel Analytics";
