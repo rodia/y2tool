a5ba0eb397027c0029e78dd93ad5e3fd8911bba9
luis <luis@sozo>
2013-07-15 17:49:50 -0400

    analytics

--- application/views/admin/list-channels-analytics.php
+++ application/views/admin/list-channels-analytics.php
@@ -1,10 +1,16 @@
+<<script type="text/javascript">
+$(document).ready(function(){
+	$("#product-table tr").toggle( "highlight" );
+});
+</script>
+
 <center>
 	<?php if ($this->input->get("msg")): ?>
 	<div class="forgot-pwd success">
 		<p><?php echo $this->input->get("msg"); ?></p>
 	</div>
 	<?php endif; ?>
-    <?php echo form_open('video/bulkActions', array('id' => 'bulk-form', 'name' => 'bulk-form', 'method' => 'post')); ?>
+    
 	<div class="step-user">
 		<table border="0" width="100%" cellpadding="0" cellspacing="0" id="product-table">
 			<tr>
@@ -24,18 +30,12 @@
 				<?php $checked = in_array($row->id, $hold_users); ?>
 				<?php $hold_username[$row->id] = $row->user_login; ?>
 					<tr<?php echo ($key % 2) ? " class=\"alternate-row\"" : ""; ?>>
-						<td><input type="checkbox" name="ids[]" value="<?php echo $row->id ?>" title="<?php echo $row->id; ?>"<?php echo $checked ? " checked=\"checked\"" : ""; ?>></td>
-						<td>
-						<a href="<?php echo base_url(); ?>video/videos/<?php echo $row->id; ?>" ><b>Show videos</b></a><br/>
-						<a href="<?php echo base_url(); ?>video/playlist/<?php echo $row->id; ?>" ><b>Show playlist</b></a><br/>
-						<a href="<?php echo base_url(); ?>admin/upload/<?php echo $row->id; ?>" ><b>Upload video</b></a><br/></td>
 						<td title="<?php echo $row->id; ?>"><?php echo $row->user_login; ?></td>
 						<td><?php echo $row->youtube_channels; ?></td>
 						<!--<td><?php  ?></td>-->
 						<td><?php echo $row->youtube_content_category; ?></td>
 						<td><?php echo $row->country; ?></td>
 						<td><?php echo $row->sex; ?></td>
-						<td><button name="selecting" class="add-remove <?php echo $checked ? "remove" : "padding-button"; ?>" title="<?php echo $row->id; ?>"><?php echo $checked ? "Remove" : "Add"; ?></button></td>
 					</tr>
 				<?php endforeach; ?>
 			<?php endif; ?>
@@ -48,7 +48,5 @@
 		<a href="#" id="get-videos">Show videos of user selected</a>
 		<div id="content-dinamic-show-videos"></div>
 	</div>
-	<?php echo form_hidden("selected-action"); ?>
-	<?php echo form_hidden("users[]"); ?>
-    <?php echo form_close(); ?>
+	
 </center>
