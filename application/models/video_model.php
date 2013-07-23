a61e4182ff53aeabe96da99427a41701475aa8d5
César Israel Jaldin Peña <rodia.pierdra@gmail.com>
2013-07-22 22:07:56 -0400

    Fix upload video

--- application/models/video_model.php
+++ application/models/video_model.php
@@ -536,7 +536,7 @@ class Video_model extends CI_Model {
 	 * @return boolean
 	 */
 	public function load_video($file_video) {
-//		$config['upload_path'] = $this->config->item("upload_path");;
+		$config['upload_path'] = $this->config->item("upload_path");;
 //		$config['allowed_types'] = $this->config->item("allowed_types_video");
 //		$config['max_size']	= $this->config->item("max_size_video");
 
