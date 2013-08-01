695e8db03be355e2198f706f0fc53c18d25e6cdc
César Israel Jaldin Peña <rodia.pierdra@gmail.com>
2013-08-01 19:06:09 -0400

    Fix remove play list

--- application/views/admin/playlist.php
+++ application/views/admin/playlist.php
@@ -53,7 +53,7 @@ if (!defined('BASEPATH'))
                     <a href="<?php echo base_url(); ?>video/videolist/<?php echo $user_id; ?>/<?php echo $playlistListEntry["id"]; ?>" ><b>Show videos</b></a><br/>
                     <a href="<?php echo base_url(); ?>video/add_video_playlist/<?php echo $user_id; ?>/<?php echo $playlistListEntry["id"]; ?>" ><b>Add video</b></a><br/>
                     <a href="<?php echo base_url(); ?>video/edit_playlist/<?php echo $user_id; ?>/<?php echo $playlistListEntry["id"]; ?>" ><b>Edit playlist</b></a><br/>
-                    <a href="<?php echo base_url(); ?>video/delplaylist/<?php echo $user_id; ?>/<?php echo $playlistListEntry["id"]; ?>" ><b>Remove playlist</b></a><br/>
+                    <a href="<?php echo base_url(); ?>video/delplaylist/<?php echo $user_id; ?>/<?php echo $playlistListEntry["id"]; ?>" onclick="return confirm('Are you sure remove this playlist?');"><b>Remove playlist</b></a><br/>
                 </td>
             </tr>
             <?php endforeach; ?>
