0be7acb7167f85d0df41837b9ec1fcce7293a58f
luis <luis@sozo>
2013-08-01 12:09:17 -0400

    featured channel

--- application/models/video_model.php
+++ application/models/video_model.php
@@ -1857,11 +1857,11 @@ class Video_model extends CI_Model {
 				$yt_base_return = $youtube_base->channels->listChannels('id,brandingSettings', array(
 					'id' => $this->user_model->get_user_meta($user_id, 'channelID', true),
 				));
-				
+				return $yt_base_return;
 				/*foreach($youtube_base['items'] as $youtube_base_channel){
 
 				}*/
-
+		
 				foreach($yt_base_return['items'] as $youtube_base_channel){
 				
 					$tmp_ob = new Google_Channel();
